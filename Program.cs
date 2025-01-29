4f6d38980d42afc240f50575c9dfb329193935f6
Admin <Admin@ACER>
2025-01-29 13:51:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+* Author: Sheetal Rana
+* Date: 29 January 2025
+* Project: Task_1
+* Description:
+* Calculate total money spent and average price per trip for Carlo's business travels.
+*/
+
+using System;
+
+namespace AssignmentOneTaskOne
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
